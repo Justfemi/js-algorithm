d3ff150d77d4abfdf4dcdbf1102ff3fb665c0c5c
AJIBADE Olufemi <62211699+Justfemi@users.noreply.github.com>
2021-05-21 19:17:23 +0100

    Update task.js

--- task.js
+++ task.js
@@ -60,31 +60,64 @@ function convertFahrToCelsius (num) {
 
 function checkYuGiOh(n) {
     convertN = isNaN(n);
+    checkType = typeof(n);
     let arr = [];
-    if(convertN) {
+    
+    if(checkType === 'string') {
+      if(convertN) {
+        let ans = `invalid parameter: "${n}"`;
+        console.log(ans);
+        return ans;
+      } else {
+          for (var i = 1; i <= n; i++)
+          if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0) {
+              arr.push("yu-gi-oh")
+          } else if (i % 2 == 0 && i % 3 == 0) {
+              arr.push("yu-gi")
+          } else if (i % 2 == 0 && i % 5 == 0) {
+              arr.push("yu-oh")
+          } else if (i % 3 == 0 && i % 5 == 0) {
+              arr.push("gi-oh")
+          } else if (i % 2 == 0) {
+              arr.push("yu")
+          } else if (i % 3 == 0) {
+              arr.push("gi")
+          } else if (i % 5 == 0) {
+              arr.push("oh")
+          } else {
+              arr.push(i)
+          }
+      console.log(arr);
+      return arr;
+      }
+    } else if (checkType === 'number') {
+          for (var i = 1; i <= n; i++)
+          if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0) {
+              arr.push("yu-gi-oh")
+          } else if (i % 2 == 0 && i % 3 == 0) {
+              arr.push("yu-gi")
+          } else if (i % 2 == 0 && i % 5 == 0) {
+              arr.push("yu-oh")
+          } else if (i % 3 == 0 && i % 5 == 0) {
+              arr.push("gi-oh")
+          } else if (i % 2 == 0) {
+              arr.push("yu")
+          } else if (i % 3 == 0) {
+              arr.push("gi")
+          } else if (i % 5 == 0) {
+              arr.push("oh")
+          } else {
+              arr.push(i)
+          }
+      console.log(arr);
+      return arr;
+    } else if(checkType === 'boolean') {
         let ans = `invalid parameter: "${n}"`;
         console.log(ans);
         return ans;
     } else {
-        for (var i = 1; i <= n; i++)
-        if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0) {
-            arr.push("yu-gi-oh")
-        } else if (i % 2 == 0 && i % 3 == 0) {
-            arr.push("yu-gi")
-        } else if (i % 2 == 0 && i % 5 == 0) {
-            arr.push("yu-oh")
-        } else if (i % 3 == 0 && i % 5 == 0) {
-            arr.push("gi-oh")
-        } else if (i % 2 == 0) {
-            arr.push("yu")
-        } else if (i % 3 == 0) {
-            arr.push("gi")
-        } else if (i % 5 == 0) {
-            arr.push("oh")
-        } else {
-            arr.push(i)
-        }
-    console.log(arr);
-    return arr;
+        let ans = `invalid parameter: "${n}"`;
+        console.log(ans);
+        return ans;
     }
 }
